97da1b19721803520fe28c7df63fbad1c66de508
minaton-ru <minaton@yandex.ru>
2022-02-01 10:15:47 +0300

    валидация входных данных

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # API на DRF для подсчета месячного процента по депозиту  
 
 ## Запуск проекта  
-Скопировать файлы  
+1. Скопировать файлы  
+2. Создать файл .env для переменных окружения DEBUG и SECRET_KEY  
+3. Запустить сервер:  
 ```pip install -r requirements.txt```  
 ```python manage.py makemigrations```  
 ```python manage.py migrate```  
 ```python manage.py runserver```   
-Создать файл .env для переменных окружения DEBUG и SECRET_KEY  
+4. Открыть `http://127.0.0.1:8000/calculate/` и создать POST-запрос в форме Django  
+3. Или отправить POST-запрос на этот URL через Postman, файл коллекции `python-deposit-API.postman_collection.json`  
 
 ## Описание API  
 Метод POST  
@@ -23,5 +26,8 @@ URL - `http://127.0.0.1:8000/calculate/`
 Возвращается поле deposit созданной записи: словарь { 'последняя дата месяца': сумма вклада с накопленным процентом }  
   
 ## TODO:  
-- проверка валидности входных данных  
+- unit-тесты  
+
+
+![Postman](https://s1.hostingkartinok.com/uploads/images/2022/02/14fbd20e7a7ee9f6cc5c69d9c856bf64.png)  
   
--- depositapi/views.py
+++ depositapi/views.py
@@ -1,4 +1,3 @@
-from depositapi.models import Deposit  
 from depositapi.serializers import DepositSerializer  
 from rest_framework import generics
 from rest_framework import status
@@ -9,21 +8,28 @@ from dateutil.relativedelta import relativedelta
 class DepositCalculate(generics.CreateAPIView):
     serializer_class = DepositSerializer
     def post(self, request):
-        newdeposit = DepositSerializer(data=request.data)
-        if newdeposit.is_valid():
-            date = request.data.get('date')
-            periods = int(request.data.get('periods'))
-            amount = int(request.data.get('amount'))
-            rate = float(request.data.get('rate'))
-            deposit = {}
-            for i in range(periods):
-                amount = amount * (1 + rate/12/100) # к сумме вклада добавляется сумма по процентам за этот месяц
-                deposit[date] = round(amount, 2) # округляем число до двух знаков после запятой
-                date_time_obj = datetime.datetime.strptime(date, '%d.%m.%Y') # переводим дату-строку в объект календарной даты
-                date_time_obj = date_time_obj + relativedelta(months=+1, day=31) # увеличиваем на один месяц и добавляем максимальное количество дней в месяце, чтобы получить последний день месяца
-                date = datetime.datetime.strftime(date_time_obj, '%d.%m.%Y') # форматируем дату обратно в строку
-            # newdeposit = Deposit(date=date, periods=periods, amount=amount, rate=rate, deposit=deposit)
-            newdeposit.deposit = deposit
-            newdeposit.save
-            return Response(newdeposit.deposit, status=status.HTTP_201_CREATED)
-        return Response(newdeposit.errors, status=status.HTTP_400_BAD_REQUEST)
+        error_message = { 'error': 'Описание ошибки'}
+        date = request.data.get('date')
+        date = request.data.get('date')
+        periods = int(request.data.get('periods'))
+        amount = int(request.data.get('amount'))
+        rate = float(request.data.get('rate'))
+        deposit = {}
+        try: # проверяем что дата введена в нужном формате
+            date_time_obj = datetime.datetime.strptime(date, '%d.%m.%Y') # переводим дату-строку в объект календарной даты
+        except ValueError:
+            return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            if 1<=periods<=60 and 10000<=amount<=3000000 and 1<=rate<=8: # валидация входных данных
+                newdeposit = DepositSerializer(data=request.data)
+                for i in range(periods):
+                    amount = amount * (1 + rate/12/100) # к сумме вклада добавляется сумма по процентам за этот месяц
+                    deposit[date] = round(amount, 2) # округляем число до двух знаков после запятой  
+                    date_time_obj = date_time_obj + relativedelta(months=+1, day=31) # увеличиваем на один месяц и добавляем максимальное количество дней в месяце, чтобы получить последний день месяца
+                    date = datetime.datetime.strftime(date_time_obj, '%d.%m.%Y') # форматируем дату обратно в строку
+                # newdeposit = Deposit(date=date, periods=periods, amount=amount, rate=rate, deposit=deposit)
+                newdeposit.deposit = deposit
+                newdeposit.save
+                return Response(newdeposit.deposit, status=status.HTTP_200_OK)
+            else:
+                return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
